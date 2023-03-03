644a832de43a966f3f99eb594e8d713d8b36c195
yejin0718 <a89557400@email.com>
2023-03-03 15:02:21 +0900

    feat: 환경설정

---
+++ kakaoServer/src/main/java/com/coffeorder/kakaoserver/KakaoServerApplication.java
@@ -0,0 +1,13 @@
+package com.coffeorder.kakaoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoServerApplication.class, args);
+	}
+
+}
